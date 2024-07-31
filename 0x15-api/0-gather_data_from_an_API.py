6da3ba630e78f97c38be47b4bf247f3e704478ce
Yasser <yasserdalouzi@gmail.com>
2024-07-31 14:14:17 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,6 +6,7 @@
 import requests
 import sys
 
+
 def get_employee_todo_progress(e_id):
     # Base URL for the JSONPlaceholder API
     base_url = "https://jsonplaceholder.typicode.com"
@@ -30,7 +31,7 @@ def get_employee_todo_progress(e_id):
     d_tasks = sum(1 for todo in todos if todo['completed'])
 
     # Print progress
-    print(f"Employee {e_name} is done with tasks({d_tasks}/{t_tasks}): ")
+    print(f"{e_name} is done with tasks({d_tasks}/{t_tasks}): ")
 
     # Print completed task titles
     for todo in todos:
