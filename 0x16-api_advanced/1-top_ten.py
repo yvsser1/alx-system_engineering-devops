279011e35c1c5ebcc71cc339779061c4fe770327
Yasser <yasserdalouzi@gmail.com>
2024-08-13 22:02:53 +0100

    fixing stuff

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/yvsser1)"
+       'User-Agent': 'AlxRedditApi/1.0 (by /u/yvsser1)'
     }
     params = {
         "limit": 10
