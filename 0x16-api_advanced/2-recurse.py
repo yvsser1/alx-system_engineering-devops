2c3d9823d0c73f4ae715dc5daf117a60589faa24
Yasser <yasserdalouzi@gmail.com>
2024-08-14 11:14:49 +0100

    codestyle fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,33 +4,31 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
+    """Recursively queries RedditAPI and returns a list of hotpost titles"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {
-        'User-Agent': 'AlxRedditApi/1.0 (by /u/yvsser1)'
-    }
-    params = {'limit': 100}
-    if after:
-        params['after'] = after
-    try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            if not posts:
-                return hot_list
-            for post in posts:
-                hot_list.append(post['data']['title'])
-            after = data['data']['after']
-            if after:
-                return recurse(subreddit, hot_list, after)
-            else:
-                return hot_list
-        elif response.status_code == 404:
-            return None
-        else:
-            print(f"An error occurred: Status code {response.status_code}")
-            return None
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    headers = {'User-Agent': 'AlxRedditApi/1.0 (by /u/yvsser1)'}
+    params = {'limit': 100, 'after': after}
+
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code != 200:
         return None
+
+    data = response.json()
+    posts = data['data']['children']
+    for post in posts:
+        hot_list.append(post['data']['title'])
+
+    after = data['data']['after']
+    if after:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
+
+
+if __name__ == '__main__':
+
+    result = recurse('python')
+    if result is not None:
+        print(len(result))
+    else:
+        print("None")
